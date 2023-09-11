96489122d917f79064236d8a7f6d6b9d7f143a84
Robertas Stankevičius <stankevic.robertas@gmail.com>
2023-09-11 21:57:39 +0300

    Add project files.

---
+++ RefAndOut_Exercises/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RefAndOut_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = 5;
+            //Swap(ref number);
+            //Console.WriteLine(number);
+
+            string word = Console.ReadLine();
+            TrimAndCapitalize(ref word);
+            Console.WriteLine(word);
+        }
+
+        public static void Swap(ref int number)
+        {
+            number = 99;            
+        }
+
+        public static void IncrementByN(ref int number)
+        {
+            number = number * number;            
+        }
+
+        public static void TrimAndCapitalize(ref string word)
+        {
+            word = word.Trim();
+            word = char.ToUpper(word[0]) + word.Substring(1);
+        }
+    }
+}
